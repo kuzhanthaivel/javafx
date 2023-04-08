1655b105bc0c03a477993ae5aa766a5885e5146e
kuzhanthaivelu <113583512+kuzhanthaivel@users.noreply.github.com>
2023-04-08 21:32:06 +0530

    Create rectangle_2D.java

---
+++ rectangle_2D.java
@@ -0,0 +1,37 @@
+import javafx.application.Application; 
+import javafx.scene.Group; 
+import javafx.scene.Scene; 
+import javafx.stage.Stage; 
+import javafx.scene.shape.Rectangle;
+
+public class RectangleExample extends Application { 
+   @Override 
+   public void start(Stage stage) { 
+      //Drawing a Rectangle 
+      Rectangle rectangle = new Rectangle();  
+      
+      //Setting the properties of the rectangle 
+      rectangle.setX(150.0f); 
+      rectangle.setY(75.0f); 
+      rectangle.setWidth(300.0f); 
+      rectangle.setHeight(150.0f);      
+         
+      //Creating a Group object  
+      Group root = new Group(rectangle); 
+         
+      //Creating a scene object 
+      Scene scene = new Scene(root, 600, 300);  
+      
+      //Setting title to the Stage 
+      stage.setTitle("Drawing a Rectangle"); 
+         
+      //Adding scene to the stage 
+      stage.setScene(scene); 
+         
+      //Displaying the contents of the stage 
+      stage.show(); 
+   }      
+   public static void main(String args[]){ 
+      launch(args); 
+   } 
+} 
